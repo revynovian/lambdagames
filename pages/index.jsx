ad9b48ed75014d73c368189c2534ef57786d886e
Oki03 <achmadsyauqi03@gmail.com>
2021-10-03 16:58:23 +0700

    completed components landing page

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,12 +1,12 @@
-import styles from '../styles/Home.module.css'
+import styles from "../styles/Home.module.css";
 import React from "react";
 import Thegames from "../components/home/the-games/Thegames";
-// import Requirements from "./requirements/Requirements";
-// import Features from "./features/Features";
-// import Scores from "./scores/Scores";
-// import Subscribe from "./subscribe/Subscribe";
+import Requirements from "../components/home/requirements/Requirements";
+import Features from "../components/home/features/Features";
+import Scores from "../components/home/scores/Scores";
+import Subscribe from "../components/home/subscribe/Subscribe";
 // import Footer from "../footer/Footer";
-import { Container, Row, Col, Image, Button } from "react-bootstrap";
+import { Container, Row, Col, Button } from "react-bootstrap";
 import { BiChevronsDown } from "react-icons/bi";
 import Link from "next/link";
 
@@ -14,14 +14,14 @@ const Home = () => {
   return (
     <main>
       <div className={styles.mainPage}>
-        <Container >
+        <Container>
           <Row style={{ marginTop: "120px" }} className="text-start">
             <Col>
               <h1 className={styles.textAnimation_a}>PLAY</h1>
               <h1 className={styles.textAnimation_b}>TRADITIONAL GAME</h1>
               <h2 className={styles.typing}>Experience new traditional game play</h2>
               <Link href="/games/rps/play" passHref>
-                <Button  className={`${styles.customButton} mt-5 `} variant="warning">
+                <Button className={`${styles.customButton} mt-5 `} variant="warning">
                   <strong>PLAY NOW</strong>
                 </Button>
               </Link>
@@ -31,15 +31,18 @@ const Home = () => {
             <Col>
               <p className="text">THE STORY</p>
               <a href="#games" className="rounded">
-                <BiChevronsDown className={styles.iconAnimation}/>
+                <BiChevronsDown className={styles.iconAnimation} />
               </a>
             </Col>
           </Row>
         </Container>
       </div>
-        <Thegames />
+      <Thegames />
+      <Requirements />
+      <Features />
+      <Scores />
+      <Subscribe />
     </main>
-    
   );
 };
 
